20d23534477c8eb5bca713f9d20b9b676d6a60d7
carlos marquez <marquezga.carlos@gmail.com>
2022-12-08 14:47:39 -0600

    Wait 60s

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -23,4 +23,6 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
+        self.stdout.write('Waiting special!')
+        time.sleep(60)
         self.stdout.write(self.style.SUCCESS('Database available!'))
